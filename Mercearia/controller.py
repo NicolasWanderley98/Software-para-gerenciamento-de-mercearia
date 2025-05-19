53a5050f5288bcb74de1b0e06679e74a242112ff
Nicolas Wanderley <nicolaswanderley98@gmail.com>
2025-05-19 14:07:14 -0300

    00

--- Mercearia/controller.py
+++ Mercearia/controller.py
@@ -81,7 +81,25 @@ class ControllerEstoque:
         else:
             print('Categoria inexistente')
 
+    def removerProduto(self, nome):
+        x = DaoEstoque.ler()
+        est = list(filter(lambda x: x.produto.nome == nome, x))
+        if len(est) > 0:
+            for i in range(len(x)):
+                if x[i].produto.nome == nome:
+                    del x[i]
+                    break
+            print('Produto foi removido com sucesso')
+        else:
+            print('O produto que deseja remover não existe')
+
+        with open('estoque.txt', 'w') as arq:
+            for i in x:
+                arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" +
+                               i.produto.categoria + "|" + str(i.quantidade))
+                arq.writelines('\n')
+
 
 
 a = ControllerEstoque()
-a.cadastrarProduto('banana','5','Verduras', 10)
+a.removerProduto('banana')
